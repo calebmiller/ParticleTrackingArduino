c39974b0ff693b0ea6f8a8a0f832ed5c670fc6ed
Caleb Miller <calebmiller@uvic.ca>
2025-04-04 09:33:32 -0700

    added clock and button framework

--- main/main.ino
+++ main/main.ino
@@ -1,15 +1,21 @@
 #include <Adafruit_NeoPixel.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_LEDBackpack.h>
+
 #include "line.h"
 #include "ball.h"
 #include "noise.h"
 
+// Setup for the clock
+Adafruit_7segment matrix = Adafruit_7segment();
+int clock_time=0;
 
-// Which pin on the Arduino is connected to the NeoPixels?
-// On a Trinket or Gemma we suggest changing this to 1:
+// LED string pin
 #define LED_PIN    2
 
-// How many NeoPixels are attached to the Arduino?
-#define LED_COUNT 30
+// Pixels per string
+#define LED_COUNT 100
 
 // Declare our NeoPixel strip object:
 Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
@@ -23,22 +29,51 @@ Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
 //   NEO_RGBW    Pixels are wired for RGBW bitstream (NeoPixel RGBW products)
 
 
+// Button Pins
+const int BLUE_BUTTON_PIN 5
+int blue_state=0;
+
 // setup() function -- runs once at startup --------------------------------
 
 void setup() {
+//LED SETUP
   strip.begin();           // INITIALIZE NeoPixel strip object (REQUIRED)
   strip.show();            // Turn OFF all pixels ASAP
-  strip.setBrightness(200); // Set BRIGHTNESS to about 1/5 (max = 255)
+  strip.setBrightness(255); // Set BRIGHTNESS to about 1/5 (max = 255)
   //strip.setPixelColor(10, strip.Color(255,255,255));
   //strip.show();
+
+//CLOCK SETUP
+#ifndef __AVR_ATtiny85__
+  Serial.begin(9600);
+  Serial.println("7 Segment Backpack Test");
+#endif
+  matrix.begin(0x70);
+
+//BUTTON SETUP
+  pinMode(BLUE_BUTTON_PIN, INPUT)
 }
 
 
 // loop() function -- runs repeatedly as long as board is on ---------------
 
 void loop() {
+  //Update clock:
+  clock_time+=1;
+  matrix.println(clock_time);
+  matrix.writeDisplay();
+
+  //CHECK BUTTON PUSH
+  blue_state=digitalRead(BLUE_BUTTON_PIN)
+  if(blue_state == HIGH){
+    //run blue program
+    delay(10000) //subroutine should return after animation finishes
+  }
+
   //runBall(strip);
   runNoise(strip);
   //runLine(strip);
   //delay(1000);
+
+
 }
