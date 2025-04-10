95ee632876f949b9f01e3eff8eb994624cad2f31
Caleb Miller <calebmiller@uvic.ca>
2025-04-10 10:31:28 -0700

    better noise to button push response

--- main/noise.cpp
+++ main/noise.cpp
@@ -4,24 +4,25 @@
 
 
 void runNoise(Adafruit_NeoPixel &strip) {
+
+	for(int i=0; i<10; i++){	
+		int nLights = random(10,50);
+		for(int i=0; i<nLights; i++){
+			int pixel = random(0,100);
+			int r = random(50,255);
+			int g = random(50,255);
+			int b = random(50,255);
+			strip.setPixelColor(pixel, strip.Color(r,g,b));
+		}
 	
-	int nLights = random(10,50);
-	for(int i=0; i<nLights; i++){
-		int pixel = random(0,100);
-		int r = random(50,255);
-		int g = random(50,255);
-		int b = random(50,255);
-		strip.setPixelColor(pixel, strip.Color(r,g,b));
+		strip.show();
+		delay(200);
+
+		// Turn off all the lights
+    	for (int i = 0; i < strip.numPixels(); i++) {
+        	strip.setPixelColor(i, strip.Color(0, 0, 0));
+    	}
+
+    	strip.show();
 	}
-	
-	strip.show();
-	delay(200);
-
-	// Turn off all the lights
-    for (int i = 0; i < strip.numPixels(); i++) {
-        strip.setPixelColor(i, strip.Color(0, 0, 0));
-    }
-
-    strip.show();
-
 }
