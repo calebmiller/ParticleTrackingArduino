5b58befb2bc00fc923c0c63119c4a97c56170460
Caleb Miller <calebmiller@uvic.ca>
2025-04-16 11:32:27 -0700

    cleaned up, should be good to go

--- main/led.cpp
+++ main/led.cpp
@@ -1,7 +1,6 @@
 #include "led.h"
 #include "config.h"
 
-
 void LEDSystem::initLEDs(Adafruit_NeoPixel* strips, int numStrips){
 
   this->strips = strips;
@@ -11,280 +10,99 @@ void LEDSystem::initLEDs(Adafruit_NeoPixel* strips, int numStrips){
     strips[i].show();
     strips[i].setBrightness(100);
   }
+  clear();
   int rows = 7;
   float distance = 16.0;
   generate_grid(rows, distance);
   Serial.println("Geometry Generated");
-  
-  //populate_map(points, pin_dict);
-
 }
 
-// Set the geometry of the LED system
-void LEDSystem::setGeometry(int n_rows, int n_cols, int n_pixels_per_substrip,
-                           float hex_size, float led_distance,
-                           int between_substrip_led_offset,
-                           int first_led_offset, float strip_offset_from_ground, float strip_offset_from_ceiling)
-{
-    // Validate input
-    if (n_rows <= 0 || n_cols <= 0 || n_pixels_per_substrip <= 0 || hex_size <= 0 || led_distance <= 0) {
-        //throw std::invalid_argument("Geometry parameters must be positive.");
+void LEDSystem::setVolume(coordinate coor, float radius) {
+  for(int i=0; i<460;i++){
+    int xdiff=abs(points[i].x-coor.x);
+    int ydiff=abs(points[i].y-coor.y);
+    int zdiff=abs(points[i].z-coor.z);
+    if(xdiff<radius && ydiff<radius && zdiff<radius){
+      float dist= sqrt(pow(xdiff,2)+pow(ydiff,2)+pow(zdiff,2));
+      float scale=1-(dist/radius);
+      if (scale<0) scale=0;
+      setPixel(pins[i].strip,pins[i].pixel,int(scale*255),int(scale*255),int(scale*255));
     }
-
-    // Store geometry parameters
-    this->n_rows = n_rows;
-    this->n_cols = n_cols;
-    this->n_pixels_per_substrip = n_pixels_per_substrip;
-    this->between_substrip_offset = between_substrip_led_offset;
-   // this->strip_offset_from_ground = strip_offset_from_ground;
-   // this->strip_offset_from_ceiling = strip_offset_from_ceiling;
-
-    // Store distances
-    this->hex_size = hex_size;             // Distance between nodes in the hexagonal grid
-    //this->edge_size = hex_size / 1.73205f; // Edge size of the hexagon
-    this->led_distance = led_distance;     // Distance between LEDs on a strip
-    this->first_led_offset = first_led_offset;
-
-    // Calculate offsets for the hexagonal grid
+  }
 }
-/*
-void LEDSystem::drawPixel(coordinate coor, float radius, int r, int g, int b) {
-    int x_min = static_cast<int>(std::floor(coor.x - radius));
-    int x_max = static_cast<int>(std::ceil(coor.x + radius));
-    int y_min = static_cast<int>(std::floor(coor.y - radius));
-    int y_max = static_cast<int>(std::ceil(coor.y + radius));
-    int z_min = static_cast<int>(std::floor(coor.z - radius));
-    int z_max = static_cast<int>(std::ceil(coor.z + radius));
 
-    for (int x = x_min; x <= x_max; ++x) {
-        for (int y = y_min; y <= y_max; ++y) {
-            for (int z = z_min; z <= z_max; ++z) {
-                coordinate current = {x, y, z};
-                if (coordinate_pixel_map.find(current) != coordinate_pixel_map.end()) {
-                    led_PIN_pixel led_pixel = coordinate_pixel_map[current];
-                    strips[led_pixel.PIN]->setPixelColor(led_pixel.pixel_index, r, g, b);
-                }
-            }
-        }
-    }
-}
-*/
 void LEDSystem::show() {
-    for (int i = 0; i < 7; i++) {
+  for (int i = 0; i < 7; i++) {
     strips[i].show();
   }
 }
 void LEDSystem::clear(){
-    for (int i = 0; i < 7; i++) {
-      for (int j = 0; j < LED_COUNT; j++) {
-        strips[i].setPixelColor(j, strips[i].Color(0, 0, 0));
-      }
-    strips[i].show();
+  for (int i = 0; i < 7; i++) {
+    for (int j = 0; j < LED_COUNT; j++) {
+      strips[i].setPixelColor(j, strips[i].Color(0, 0, 0));
+    }
+  strips[i].show();
   }
-}
-
-// Build the LED system
-void LEDSystem::build() {
-    buildCoordinatePixelMap();
-    // in case something here
-}
-
-
-// Build the coordinate-to-pixel map
-void LEDSystem::buildCoordinatePixelMap() {
-    //coordinate_pixel_map.clear();
-    for (int col = 0; col < n_cols; ++col) {
-        buildColumnCoordinatePixelMap(col);
-    }
 
 }
 
-void LEDSystem::buildColumnCoordinatePixelMap(int col) {
-  /*
-    int led_start = first_led_offset;
-    bool is_long_column = (col % 2 == 0); // Determine if the column is a long column
-    float x_start = is_long_column 
-                    ? edge_size + 3 * col / 2 * edge_size 
-                    : 2.5 * edge_size + (col / 2) * 3 * edge_size;
-    float y_start = is_long_column ? hex_size / 2 : hex_size;
-    bool from_ground = true;
-
-    int max_rows = is_long_column ? n_rows : n_rows - 1;
-
-    for (int row = 0; row < max_rows; ++row) {
-        float x = is_long_column ? x_start : x_start + col * edge_size * 2;
-        float y = y_start + row * hex_size;
-        float z_start = strip_offset_from_ground;
-
-        for (int i = 0; i < n_pixels_per_substrip; ++i) {
-            float z = z_start + i * led_distance;
-            int x_floor = static_cast<int>(std::floor(x));
-            int y_floor = static_cast<int>(std::floor(y));
-            int z_floor = static_cast<int>(std::floor(z));
-
-            int pixel_index = from_ground ? led_start + i : (led_start + n_pixels_per_substrip - 1 - i);
-            led_PIN_pixel led_pixel = {col, pixel_index};
-            coordinate coord = {x_floor, y_floor, z_floor};
-            coordinate_pixel_map[coord] = led_pixel;
-        }
-
-        led_start = led_start + n_pixels_per_substrip + between_substrip_offset;
-        from_ground = !from_ground; // Toggle if needed per row
-    }
-    */
-}
 void LEDSystem::generate_grid(int rows, float distance) {
-    int cnt=0;
-    for (int row = 0; row < rows; row++) {
-        int num_points = (row % 2 == 0) ? 7 : 6;
-        bool top = (row % 2 == 0);
-        int ledcnt=99;
-        for (int col = 0; col < num_points; col++) {
-            float x = col * distance * sqrt(3) / 2;
-            float y = row * distance * sqrt(3) / 2;
-            if (row % 2 == 1) {
-                x += distance * sqrt(3) / 4;
-            }
-            x += 8;
-            y += 8;
-            for(int lay=0; lay<10; lay++){
-              int ledid = top ? (ledcnt - 9 + lay) : (ledcnt - lay);
-              float z=lay*10+5;
-              coordinate pnt = {int(x), int(y), int(z)};
-              int strip_id = get_pin_id(int(y));
-              pixel_ID pxl={strip_id,ledid};
-              points[cnt]=pnt;
-              pins[cnt]=pxl;
-              cnt++;
-              /*
-              Serial.print(pnt.x);
-              Serial.print(",");
-              Serial.print(pnt.y);
-              Serial.print(",");
-              Serial.print(pnt.z);
-              Serial.print(",");
-              Serial.print(pxl.strip);
-              Serial.print(",");
-              Serial.println(pxl.pixel);
-              Serial.println("");
-              */
-            }
-            top = !top;
-            ledcnt -= 13;
-        }
+  int cnt=0;
+  for (int row = 0; row < rows; row++) {
+    int num_points = (row % 2 == 0) ? 7 : 6;
+    bool top = (row % 2 == 0);
+    int ledcnt=99;
+    for (int col = 0; col < num_points; col++) {
+      float x = col * distance * sqrt(3) / 2;
+      float y = row * distance * sqrt(3) / 2;
+      if (row % 2 == 1) x += distance * sqrt(3) / 4;      
+      x += 8;
+      y += 8;
+      for(int lay=0; lay<10; lay++){
+        int ledid = top ? (ledcnt - 9 + lay) : (ledcnt - lay);
+        float z=lay*10+5;
+        coordinate pnt = {int(x), int(y), int(z)};
+        int strip_id = get_pin_id(int(y));
+        pixel_ID pxl={strip_id,ledid};
+        points[cnt]=pnt;
+        pins[cnt]=pxl;
+        cnt++;
+      }
+      top = !top;
+      ledcnt -= 13;
     }
+  }
 }
 int LEDSystem::get_pin_id(int y) {
-    const int keys[] = {8, 21, 35, 49, 63, 77, 91}; //check these!!!!
-    const int values[] = {2, 3, 4, 5, 6, 7, 8};
-    for (int i = 0; i < 7; i++) {
-        if (keys[i] == y) {
-            return values[i];
-        }
-    }
-    return -1; // Return -1 if the key is not found
-}
-
-/*
-void LEDSystem::populate_map(std::vector<coordinate> points, std::map<float, int> pin_dict) {
-    
-    
-    for (auto& [index, point] : points) {
-      int cols = points.size() /10; 
-      bool top = false;
-      if(cols == 7) top=true;
-      for(int i=0; i<cols; ++i){
-        for(int j=0; j<10; j++){
-          int ledid = top ? (ledcnt - 9 + j) : (ledcnt - j);
-          pixel_ID px = {pin_dict[point.y],ledid};
-          coordinate_pixel_map[point] = px;
-        }
-        top = !top;
-        ledcnt -= 13;
-      }
-    }
-
-
-
-
-
-    std::map<float, std::map<int, coordinate>> points_dict;
-    for (auto point = points.begin(); point != points.end(); ++point){
-        float y_value = round(point->second.y);
-        if (points_dict.find(y_value) == points_dict.end()) {
-            points_dict[y_value] = std::map<int, coordinate>();
-        }
-        int point_id = points_dict[y_value].size();
-        points_dict[y_value][point_id] = point->second;
-    }
-
-    int ledid = 99;
-    bool top = false;
-    for (auto& [y_value, point_map] : points_dict) {
-        int cols = point_map.size() / 10;
-        if (cols == 7) top = true;
-        for (int i = 0; i < cols; i++) {
-            for (int j = 0; j < 10; j++) {
-                int oldid = 10 * i + j;
-                coordinate coord = point_map[oldid];
-                int newid = top ? (ledid - 9 + j) : (ledid - j);
-                pixel_ID newpx = {pin_dict[y_value],newid};
-                coordinate_pixel_map[coord] = newpx;
-            }
-            top = !top;
-            ledid -= 13;
-        }
+  const int keys[] = {8, 21, 35, 49, 63, 77, 91}; //check these!!!!
+  const int values[] = {2, 3, 4, 5, 6, 7, 8};
+  for (int i = 0; i < 7; i++) {
+    if (keys[i] == y) {
+      return values[i];
     }
   }
-    */
+  Serial.print("No Valid Key found");
+  return -1; // Return -1 if the key is not found
+}
+
 
 void LEDSystem::printMap(){
   // Print the map for verification
   Serial.println("print map");
   for (int i=0; i<460; ++i) {
-        Serial.print("Coordinate: ");
-        Serial.print(points[i].x);
-        Serial.print(", ");
-        Serial.print(points[i].y);
-        Serial.print(", ");
-        Serial.print(points[i].z);
-        Serial.print(" -> Pixel ID: ");
-        Serial.print(pins[i].strip);
-        Serial.print(", ");
-        Serial.println(pins[i].pixel);
-    }
+    Serial.print("Coordinate: ");
+    Serial.print(points[i].x);
+    Serial.print(", ");
+    Serial.print(points[i].y);
+    Serial.print(", ");
+    Serial.print(points[i].z);
+    Serial.print(" -> Pixel ID: ");
+    Serial.print(pins[i].strip);
+    Serial.print(", ");
+    Serial.println(pins[i].pixel);
+  }
 }
 void LEDSystem::setPixel(int strip, int pixel, int r, int g, int b){
-  Serial.print(strip);
-  Serial.print(", ");
-  Serial.print(pixel);
-  Serial.print(", ");
-  Serial.print(r);
-  Serial.print(", ");
-  Serial.print(g);
-  Serial.print(", ");
-  Serial.print(b);
-  Serial.println();
   strips[strip].setPixelColor(pixel, strips[strip].Color(r,g,b));
-}
-//void LEDSystem::runNoise(Adafruit_NeoPixel* strips){
-void LEDSystem::runNoise(){
-  for(int i=0; i<10; i++){
-    //for(int j=0; j<7; j++){
-      int nLights = random(10,30);
-		  for(int k=0; k<nLights; k++){
-			  int pixel = random(0,100);
-			  int r = random(50,255);
-			  int g = random(50,255);
-			  int b = random(50,255);
-        strips[0].setPixelColor(pixel, strips[0].Color(r,g,b));
-		  }
-    //}
-		show();
-    delay(100);
-
-		// Turn off all the lights
-		clear();
-	}
 }
 
--- main/led.h
+++ main/led.h
@@ -3,85 +3,26 @@
 
 #include <Arduino.h>
 #include <Adafruit_NeoPixel.h>
+#include "config.h"
 
-struct coordinate{
-  int x;
-  int y;
-  int z;
-  
-  bool operator<(const coordinate& other) const {
-    if (x != other.x) return x < other.x;
-    if (y != other.y) return y < other.y;
-    return z < other.z;
-  }
-};
-
-struct pixel_ID
-{
-  int strip;
-  int pixel;
-};
 
 class LEDSystem {
-    public:
-        void initLEDs(Adafruit_NeoPixel* strips, int numStrips);
-        // Add a strip to the system
-        void addStrip(Adafruit_NeoPixel &strip);
+  public:
+    void initLEDs(Adafruit_NeoPixel* strips, int numStrips);
+    void clear();
+    void show();
+    void printMap();
+    void setPixel(int strip, int pixel, int r, int g, int b);
+    void setVolume(coordinate coor, float radius);
 
-		    //load geometry map from csv
-		    void loadGeometry();
+  private:
+    Adafruit_NeoPixel* strips;
+    int numStrips;
+    coordinate points[460];
+    pixel_ID pins[460];
 
-        // Set the geometry of the LED system
-        // n_rows, n_cols, n_pixels_per_substrip are setting the grid structures
-        // hex_space, led_distance are setting real world metric in cm. hex space is the distance between two centers of the hexagons
-        // between_substrip_led_offset: # of led not used between sub-strips
-        // first_led_offset: # of led not used before the first led, 
-        // strip_offset: the distance between the ceiling and the first led, used for making coordinate
-        void setGeometry(int n_rows, int n_cols, int n_pixels_per_substrip,
-                        float hex_size, float led_distance,
-                        int between_substrip_led_offset, 
-                        int first_led_offset, float strip_offset_from_ground, float strip_offset_from_ceiling);
-
-        // Build the LED system
-        void build();
-
-        void clear();
-
-        // Get the number of strips
-        int stripCount();
-        //void drawPixel(coordinate coor, float radius, int r, int g, int b);
-        void show();
-        void printMap();
-        void setPixel(int strip, int pixel, int r, int g, int b);
-        //void runNoise(Adafruit_NeoPixel* strips);
-        void runNoise();
-
-    private:
-        // Attributes
-        Adafruit_NeoPixel* strips;
-        int numStrips;
-        coordinate points[460];
-        pixel_ID pins[460];
-
-        // Pixels per string
-
-        int n_rows = 0;                     // Number of rows in the LED grid
-        int n_cols = 0;                     // Number of columns in the LED grid
-        int n_pixels_per_substrip = 0;      // Number of pixels per sub-strip
-        int between_substrip_offset = 0;    // Offset between sub-strips
-        int strip_offset = 0;               // Offset between strips
-        float hex_size = 0.0f;              // Distance between nodes in the hexagonal grid
-        float led_distance = 0.0f;          // Distance between LEDs on a strip
-        float horizontal_offset = 0.0f;     // Horizontal offset between nodes
-        float vertical_offset = 0.0f;       // Vertical offset between nodes
-        int first_led_offset = 0;           // Number of LEDs not used before the first LED
-
-        // Helper function to build the coordinate-to-pixel map
-        void buildCoordinatePixelMap();
-        void buildColumnCoordinatePixelMap(int col);
-        void generate_grid(int rows, float distance);
-        //void populate_map(std::map<int, coordinate> points, std::vector<float> z_coords, std::map<float, int> pin_dict);
-        int get_pin_id(int yval);
+    void generate_grid(int rows, float distance);
+    int get_pin_id(int yval);
         
 };
 
