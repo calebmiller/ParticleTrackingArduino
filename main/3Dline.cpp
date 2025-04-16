0663106c8c370e1e914eb9f4b97f9f8f18da3faa
Caleb Miller <calebmiller@uvic.ca>
2025-04-16 13:00:23 -0700

    working track

--- main/3Dline.cpp
+++ main/3Dline.cpp
@@ -4,87 +4,90 @@
 #include <Adafruit_NeoPixel.h>
 
 
-void runTrack(Adafruit_NeoPixel &strip) {
-    int face = random(0, 6); // bottom, left, back, right, front, top
-    int startx = random(0, 100); // coordinate x1 in cm
-    int starty = random(0, 100); // coordinate y1 in cm
-    int endx = random(0, 100); // coordinate x2 in cm
-    int endy = random(0, 100); // coordinate y2 in cm
+void runTrack(LEDSystem* ledControl) {
+  int face = random(0, 6); // bottom, left, back, right, front, top
+  int startx = random(0, 100); // coordinate x1 in cm
+  int starty = random(0, 100); // coordinate y1 in cm
+  int endx = random(0, 100); // coordinate x2 in cm
+  int endy = random(0, 100); // coordinate y2 in cm
 
-    int startz = 0; // Initialize z coordinate for start point
-    int endz = 0; // Initialize z coordinate for end point
+  int startz = 0; // Initialize z coordinate for start point
+  int endz = 0; // Initialize z coordinate for end point
 
-    // Convert face and coordinates to 3D coordinates
-    int startCoords[3];
-    int endCoords[3];
+  // Convert face and coordinates to 3D coordinates
+  coordinate startPoint;
+  coordinate endPoint;
 
 	//Set x,y,z points based on starting face
-    switch (face) {
-        case 0: // bottom
-            startCoords[0] = startx;
-            startCoords[1] = starty;
-            startCoords[2] = 0;
-            endCoords[0] = endx;
-            endCoords[1] = endy;
-            endCoords[2] = 100;
-            break;
-        case 1: // left
-            startCoords[0] = 0;
-            startCoords[1] = startx;
-            startCoords[2] = starty;
-            endCoords[0] = 100;
-            endCoords[1] = endx;
-            endCoords[2] = endy;
-            break;
-        case 2: // back
-            startCoords[0] = startx;
-            startCoords[1] = 0;
-            startCoords[2] = starty;
-            endCoords[0] = endx;
-            endCoords[1] = 100;
-            endCoords[2] = endy;
-            break;
-        case 3: // right
-            startCoords[0] = 100;
-            startCoords[1] = startx;
-            startCoords[2] = starty;
-            endCoords[0] = 0;
-            endCoords[1] = endx;
-            endCoords[2] = endy;
-            break;
-        case 4: // front
-            startCoords[0] = startx;
-            startCoords[1] = 100;
-            startCoords[2] = starty;
-            endCoords[0] = endx;
-            endCoords[1] = 0;
-            endCoords[2] = endy;
-            break;
-        case 5: // top
-            startCoords[0] = startx;
-            startCoords[1] = starty;
-            startCoords[2] = 100;
-            endCoords[0] = endx;
-            endCoords[1] = endy;
-            endCoords[2] = 0;
-            break;
-    }
+  switch (face) {
+    case 0: // bottom
+      startPoint.x = startx;
+      startPoint.y = starty;
+      startPoint.z = 0;
+      endPoint.x = endx;
+      endPoint.y = endy;
+      endPoint.z = 100;
+      break;
+    case 1: // left
+      startPoint.x = 0;
+      startPoint.y = startx;
+      startPoint.z = starty;
+      endPoint.x = 100;
+      endPoint.y = endx;
+      endPoint.z = endy;
+        break;
+    case 2: // back
+      startPoint.x = startx;
+      startPoint.y = 0;
+      startPoint.z = starty;
+      endPoint.x = endx;
+      endPoint.y = 100;
+      endPoint.z = endy;
+      break;
+    case 3: // right
+      startPoint.x = 100;
+      startPoint.y = startx;
+      startPoint.z = starty;
+      endPoint.x = 0;
+      endPoint.y = endx;
+      endPoint.z = endy;
+      break;
+    case 4: // front
+      startPoint.x = startx;
+      startPoint.y = 100;
+      startPoint.z = starty;
+      endPoint.x = endx;
+      endPoint.y = 0;
+      endPoint.z = endy;
+      break;
+    case 5: // top
+      startPoint.x = startx;
+      startPoint.y = starty;
+      startPoint.z = 100;
+      endPoint.x = endx;
+      endPoint.y = endy;
+      endPoint.z = 0;
+      break;
+  }
 
-	float distance = sqrt(pow(endCoords[0] - startCoords[0], 2) +
-                          pow(endCoords[1] - startCoords[1], 2) +
-                          pow(endCoords[2] - startCoords[2], 2));
+	float distance = sqrt(pow(endPoint.x - startPoint.x, 2) +
+                        pow(endPoint.y - startPoint.y, 2) +
+                        pow(endPoint.z - startPoint.z, 2));
 
 
-	int currentCoords[3] = {startCoords[0], startCoords[1], startCoords[2]};
+	coordinate currentCoords = {startPoint.x, startPoint.y, startPoint.z};
 	while(distance>0){//Move the current point by the STEPSIZE
-            currentCoords[0] += (endCoords[0] > currentCoords[0]) ? STEPSIZE : -STEPSIZE;
-            currentCoords[1] += (endCoords[1] > currentCoords[1]) ? STEPSIZE : -STEPSIZE;
-            currentCoords[2] += (endCoords[2] > currentCoords[2]) ? STEPSIZE : -STEPSIZE;
-			distance-=sqrt(3)*STEPSIZE;
+    currentCoords.x += (endPoint.x > currentCoords.x) ? STEPSIZE : -STEPSIZE;
+    currentCoords.y += (endPoint.y > currentCoords.y) ? STEPSIZE : -STEPSIZE;
+    currentCoords.z += (endPoint.z > currentCoords.z) ? STEPSIZE : -STEPSIZE;
+		distance-=sqrt(3)*STEPSIZE;
 	
-//			LEDController.TurnON(currentCoords,LINERADIUS); //Turn on LEDs within certain distance of current location
-			delay(5); //TIMESTEP
+    ledControl->setVolume(currentCoords, 30);
+    ledControl->show();
+    Serial.println("step");
+		delay(5); //TIMESTEP
 	}
-
+  Serial.println("done");
 	delay(2000);
+  ledControl->clear();
 }
--- main/3Dline.h
+++ main/3Dline.h
@@ -1,8 +1,9 @@
 #include <Adafruit_NeoPixel.h>
+#include "led.h"
 
 #ifndef TDLINE_H
 #define TDLINE_H
 
-void runTrack(Adafruit_NeoPixel &strip);
+void runTrack(LEDSystem* ledControl);
 
 #endif
