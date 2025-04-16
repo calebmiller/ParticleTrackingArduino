2fc5a72e32c4b1c714bd5eb29502e559486524c8
Caleb Miller <calebmiller@uvic.ca>
2025-04-16 11:54:14 -0700

    updated noise to run on all strings

--- main/noise.cpp
+++ main/noise.cpp
@@ -4,24 +4,20 @@
 
 
 void runNoise(LEDSystem* ledControl) {
-
-	for(int i=0; i<3; i++){
-    //for(int j=0; j<7; j++){
-      int nLights = random(0,5);
+	for(int i=0; i<5; i++){
+    for(int j=0; j<7; j++){
+      int nLights = random(10,30);
 		  for(int k=0; k<nLights; k++){
 			  int pixel = random(0,100);
 			  int r = random(50,255);
 			  int g = random(50,255);
 			  int b = random(50,255);
-        ledControl->setPixel(0,pixel, r,g,b);
-        Serial.println("set");
+        ledControl->setPixel(j,pixel, r,g,b);
 		  }
-    //}
-		Serial.println("update");
+    }
 		ledControl->show();
-		delay(1000);
+		delay(100);
 
-    Serial.println("clear");
 		// Turn off all the lights
 		ledControl->clear();
 	}
